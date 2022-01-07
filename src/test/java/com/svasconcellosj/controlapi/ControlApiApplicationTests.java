71c83dd3d9e3b32bd34b5958478fac60b9114870
Sergio Vasconcellos Junior <svasconcellosj@hotmail.com>
2022-01-07 10:13:08 -0300

    Iniciado projeto
    - Criado start do projeto
    - Criado conexão com o banco

---
+++ src/test/java/com/svasconcellosj/controlapi/ControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svasconcellosj.controlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
