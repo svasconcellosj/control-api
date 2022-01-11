578436d8d7dbc7a20b41c8f38d29f225632a5c2a
Sergio Vasconcellos Junior <svasconcellosj@hotmail.com>
2022-01-11 18:47:07 -0300

    Incluído origem permitida

--- src/main/java/com/svasconcellosj/controlapi/categorias/controller/CategoriaController.java
+++ src/main/java/com/svasconcellosj/controlapi/categorias/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaService categoriaService;
 
+	@CrossOrigin(maxAge = 10, origins = { "http://localhost:4200"} )
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaModel>> buscaCategorias() {
 		List<CategoriaModel> lista = categoriaService.buscaTodos();
